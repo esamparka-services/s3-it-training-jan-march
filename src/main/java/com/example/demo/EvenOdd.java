465070b10ebf7200e1ad6594605e41ef87646139
Kailash Dhakal <dhakalkailash1997@gmail.com>
2021-02-13 13:28:33 -0600

    First assesment

---
+++ src/main/java/com/example/demo/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import lombok.Data;
+
+@Data
+public class EvenOdd {
+
+        private int number;
+        private int odd;
+        private int even;
+        private String result;
+
+}
---
+++ src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import lombok.Data;
+
+@Data
+public class Laptop {
+    private String name;
+    private String model;
+    private String year;
+    private String price;
+    private String serialNumber;
+
+}
