b00590f4d4d1a5ae9e065f0332b73216010715b5
renushrestha <renustha18@gmail.com>
2021-02-13 17:34:56 -0600

    My Assessment

---
+++ src/main/java/com/assessment/AssessmentService.java
@@ -0,0 +1,19 @@
+package com.assessment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssessmentService {
+
+    public String ifOddOrEven(int number) {
+
+        if (number % 2 == 0) {
+            return "even";
+        }
+        else{
+            return "odd";
+        }
+
+
+    }
+}
---
+++ src/main/java/com/assessment/MyAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAssessmentApplication.class, args);
+    }
+
+}
