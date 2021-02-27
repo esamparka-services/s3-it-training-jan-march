5e919958fdb0db86aec5b2de83f6a856c7c7bd6d
shahisandeep <shahi.sandeep3@gmail.com>
2021-02-27 13:08:43 -0600

    ExceptionHandling

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.model.Palindrome;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,9 +14,33 @@ class DemoApplicationTests {
 
     @Test
     void myFirstUnitTest() {
-		int x = 10;
-		int y = 5 +5;
-		System.out.println(x==y);
+        Palindrome palindrome = new Palindrome();
+        palindrome.setString("sandeep");
+        StringBuffer reversedStringBuffer = new StringBuffer();
+        for (int x = palindrome.getString().length() - 1; x >= 0; x--) {
+            char reversedChar = (palindrome.getString().charAt(x));
+            reversedStringBuffer.append(reversedChar);
+        }
+        System.out.println(reversedStringBuffer);
+
+    }
+
+    @Test
+    public void testVar() {
+        int actual = 0;
+        try {
+            actual = test1(11);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(20, actual);
+    }
+
+    private int test1(int x) throws Exception {
+        if (x == 10) {
+            throw new Exception();
+        }
+        return 10 + 10;
     }
 
 }
