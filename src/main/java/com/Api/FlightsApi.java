fea8951eca3aee6c0ca2084f8a0157ab0ba535a7
Mukesh1939 <76882569+Mukesh1939@users.noreply.github.com>
2021-03-14 00:22:16 -0600

    tried to create the deleteRecord api by Id, tested it worked fine.

--- src/main/java/com/Api/FlightsApi.java
+++ src/main/java/com/Api/FlightsApi.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -29,8 +30,19 @@ public class FlightsApi {
         flightsRepository.save(market);
 
 
-              ResponseEntity<HttpStatus> rs=  new ResponseEntity<>(HttpStatus.CREATED);
+              ResponseEntity<HttpStatus> rs=  new ResponseEntity<>(HttpStatus.CONTINUE);
+              System.out.println(HttpStatus.CONTINUE + " " +HttpStatus.CHECKPOINT);
               return  rs;
+
+    }
+
+    @PostMapping("/deleteRecord")
+
+    public void deleteRecord (@RequestParam int id)
+    {
+
+        flightsRepository.deleteById(id);
+
 
     }
 }
